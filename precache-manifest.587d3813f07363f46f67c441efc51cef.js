57f3a4aca285c261ef83d25fab36b257b815ff75
ali <ali008009@yahoo.com>
2020-04-17 00:47:14 +0100

    Updates

---
+++ precache-manifest.587d3813f07363f46f67c441efc51cef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de820a5e51e4243710666a0684737884",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "29d569682426f8b163d2",
+    "url": "/weatherApp/static/css/main.118086a1.chunk.css"
+  },
+  {
+    "revision": "bf04991b836c837de480",
+    "url": "/weatherApp/static/js/2.2e909309.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherApp/static/js/2.2e909309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d569682426f8b163d2",
+    "url": "/weatherApp/static/js/main.2cf8c270.chunk.js"
+  },
+  {
+    "revision": "b7d0c294d64ce2665b92",
+    "url": "/weatherApp/static/js/runtime-main.46fd76e6.js"
+  },
+  {
+    "revision": "c4e83c2a815bcd509453109a8bfcdf33",
+    "url": "/weatherApp/static/media/cold.c4e83c2a.jpg"
+  },
+  {
+    "revision": "b8e5d48d44126a11a1aaabd807d1bbbc",
+    "url": "/weatherApp/static/media/hot.b8e5d48d.jpg"
+  }
+]);
